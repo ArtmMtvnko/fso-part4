d00fc4a0790c3c29382ec5416ebbe8f60fe5c52c
Artem <artemm3note@gmail.com>
2024-05-07 22:36:30 +0300

    Initialized project. Created basic file structure.

---
+++ blog-list/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+require('dotenv').config()
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
